396515500c67ce8cb3d38574ee19eb7b6e3f2623
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-08-22 08:23:17 +0300

    ✨ feat: if-else statements

--- GettingStartedWithJava/CalcEngine/src/com/inventrohyder/calcengine/Main.java
+++ GettingStartedWithJava/CalcEngine/src/com/inventrohyder/calcengine/Main.java
@@ -2,6 +2,29 @@ package com.inventrohyder.calcengine;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double value1 = 10;
+        double value2 = 10;
+        double result;
+
+        char opCode = 'a';
+
+        result = calculate(value1, value2, opCode);
+
+        System.out.println(result);
+    }
+
+
+    static double calculate(double value1, double value2, char opCode) {
+        if (opCode == 'a')
+            return value1 + value2;
+        else if (opCode == 's') {
+            return value1 - value2;
+        } else if (opCode == 'm') {
+            return value1 * value2;
+        } else if (opCode == 'd') {
+            return value1 / value2;
+        } else {
+            return 0.00d;
+        }
     }
 }
